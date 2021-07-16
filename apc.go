9b3e6d8d51917d6421230c5020c8f2380de8d500
Nathan Crapo <nathan_crapo@yahoo.com>
2021-07-15 20:50:51 -0600

    Initial main package commit

---
+++ apc.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/docopt/docopt-go"
+)
+
+func main() {
+	usage := `apc - Control APC network power strip
+
+Usage:
+  apc.py [options] (on [<port>] | off [<port>] | reset [<port>] | list)
+  apc.py [options] set-alias <name> <num>
+  apc.py [options] rm-alias <name>
+  apc.py [options] set-host <hostname>
+  apc.py --help
+
+Commands:
+  on                     Turn port on [defaults to last port if empty]
+  off                    Turn port off [default to last port if empty]
+  reset                  Reset port [default to last port if empty]
+  list                   List all ports, their aliases, and their status
+  set-alias              Set an alias for a port number
+  rm-alias               Remove alias for a port
+  set-host               Set host of APC device via IP address or hostname
+  --help                 Print this usage screen
+
+Options:
+  --config <filename>    Point to custom config file [default: ~/.config/apc/config]`
+
+	args, _ := docopt.ParseDoc(usage)
+	fmt.Println(args)
+}
