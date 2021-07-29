7b169bea08aff8a028a6090cc46631ed74147097
Nathan Crapo <nathan_crapo@yahoo.com>
2021-07-29 04:50:41 +0000

    Added telnet control to the mix

--- apc_connector.go
+++ apc_connector.go
@@ -4,7 +4,16 @@ import (
 	"errors"
 	"fmt"
 	"strconv"
+	"time"
+	"log"
+	"github.com/ziutek/telnet"
 )
+
+const OUTLET_MANAGER = "1"
+const CONTROL_PORT = "1"
+const PORT_ON_COMMAND = "1"
+const PORT_OFF_COMMAND = "2"
+const PORT_RESET_COMMAND = "3"
 
 type ApcConnector struct {
 	config *ConfigFile
@@ -22,9 +31,9 @@ func (apc *ApcConnector) On(port string) (err error) {
 		return err
 	}
 	fmt.Println("Turning on port:", num)
-	// If successful - update the last port
 	apc.config.SetLastPort(num)
-	return nil
+	err = apc.controlOutlet(num, PORT_ON_COMMAND)
+	return err
 }
 
 func (apc *ApcConnector) Off(port string) (err error) {
@@ -33,9 +42,9 @@ func (apc *ApcConnector) Off(port string) (err error) {
 		return err
 	}
 	fmt.Println("Turning off port:", num)
-	// If successful - update the last port
 	apc.config.SetLastPort(num)
-	return nil
+	err = apc.controlOutlet(num, PORT_OFF_COMMAND)
+	return err
 }
 
 func (apc *ApcConnector) Reset(port string) (err error) {
@@ -44,9 +53,9 @@ func (apc *ApcConnector) Reset(port string) (err error) {
 		return err
 	}
 	fmt.Println("Reset port:", num)
-	// If successful - update the last port
 	apc.config.SetLastPort(num)
-	return nil
+	err = apc.controlOutlet(num, PORT_RESET_COMMAND)
+	return err
 }
 
 func (apc *ApcConnector) portNumFromString(port string) (num int, err error) {
@@ -67,4 +76,63 @@ func (apc *ApcConnector) portNumFromString(port string) (num int, err error) {
 
 	// Couldn't do anything with the port string
 	return 0, errors.New("Unable to decode port '" + port + "'")
+}
+
+func (apc *ApcConnector) controlOutlet(port int, command string) (err error) {
+	server := apc.config.Hostname + ":23"
+
+	t, err := telnet.Dial("tcp", server)
+	if err != nil {
+		//log.Fatalln("Error:", err)
+		return err
+	}
+
+	t.SetUnixWriteMode(true)
+	expect(t, "User Name :")
+	sendln(t, apc.config.User)
+	expect(t, "Password  :")
+	sendln(t, apc.config.Password)
+
+	expect(t, "<ESC>")
+	expect(t, ">")
+	sendln(t, OUTLET_MANAGER)
+
+	expect(t, "<ESC>")
+	expect(t, ">")
+	sendln(t, strconv.Itoa(port))
+
+	expect(t, "<ESC>")
+	expect(t, ">")
+	sendln(t, CONTROL_PORT)
+
+	expect(t, "<ESC>")
+	expect(t, ">")
+	sendln(t, command)
+
+	expect(t, "cancel :")
+	sendln(t, "YES")
+
+	return nil
+}
+
+const timeout = 10 * time.Second
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln("Error:", err)
+	}
+}
+
+func expect(t *telnet.Conn, d ...string) {
+	checkErr(t.SetReadDeadline(time.Now().Add(timeout)))
+	checkErr(t.SkipUntil(d...))
+}
+
+func sendln(t *telnet.Conn, s string) {
+	checkErr(t.SetWriteDeadline(time.Now().Add(timeout)))
+	buf := make([]byte, len(s)+1)
+	copy(buf, s)
+	buf[len(s)] = '\n'
+	_, err := t.Write(buf)
+	checkErr(err)
 }
